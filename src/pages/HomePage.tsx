a22c577ff7a8262a90c266ffc2a8008537a258a0
MrFxde <juan.space14@gmail.com>
2024-04-19 10:35:15 -0400

    fixed button on mobile

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ const HomePage = () => {
             <div className="rounded-md shadow">
               <Link
                 to="/form"
-                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+                className="mt-5 items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
               >
                 Get Started
               </Link>
